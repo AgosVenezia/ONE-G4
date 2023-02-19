731a8cc908c7ce7b0489b19d1e34c68e18ac8e73
Agostina Celina Venezia Napolillo <agostina.cv@gmail.com>
2023-02-19 15:27:51 -0300

    Java OO: Entendiendo la Orientación a Objetos

--- 5-Java/proyecto-bytebank/src/Cuenta.java
+++ 5-Java/proyecto-bytebank/src/Cuenta.java
@@ -5,4 +5,38 @@ class Cuenta {
     // int agencia = 15; // inicializa con 15 en todas las instancias
     int numero;
     String titular;
+
+    // No retorna valor (void)
+    public void depositar(double valor) {
+        // Esta cuenta  Esta cuenta  Parámetro
+        // This account This account
+        // This object
+        // this.salo  = this.saldo  +  valor
+        // saldo = saldo + valor; 
+        // this.saldo = this.saldo + valor;
+        this.saldo += valor;
+    }
+
+    // Retorna valor (boolean)
+    public boolean retirar(double valor) {
+        if (this.saldo >= valor) {
+            //this.saldo = this.saldo - valor;
+            this.saldo -= valor;
+            return true;
+        //} else {
+            //return false;
+        }
+        return false;
+    }
+
+    public boolean transferir(double valor, Cuenta cuenta) {
+        if (this.saldo >= valor) {
+            this.saldo = this.saldo - valor;
+            //cuenta.saldo = cuenta.saldo + valor;
+            cuenta.depositar(valor);
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
