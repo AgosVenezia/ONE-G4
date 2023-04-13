0f781a67e18df1b82785668021c60edf643506b9
Agostina Celina Venezia Napolillo <agostina.cv@gmail.com>
2023-04-13 16:39:14 -0300

    Java y JDBC: Trabajando con una Base de Datos

--- 9-Java-JDBC/control-de-stock/src/main/java/com/alura/jdbc/controller/ProductoController.java
+++ 9-Java-JDBC/control-de-stock/src/main/java/com/alura/jdbc/controller/ProductoController.java
@@ -3,6 +3,13 @@ package com.alura.jdbc.controller;
 import java.util.List;
 
 import java.sql.Connection;
+
+//import java.sql.DriverManager;
+/*
+Para acceder a una base de datos necesitamos del driver de conexión.
+JDBC significa Java DataBase Connectivity. El JDBC define una capa de abstracción entre la aplicación y el driver de la base de datos. Esta capa es compuesta de interfaces que el driver implementa.
+*/
+
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -66,6 +73,36 @@ public class ProductoController {
         //return productoDao.eliminar(id);
     //}
 
+    /*public List<Map<String, String>> listar() throws SQLException {
+        Connection con = DriverManager.getConnection(
+            "jdbc:mysql://localhost/control_de_stock?useTimeZone=true&serverTimeZone=UTC", 
+            "root", 
+            "codoacodo");
+        // Para abrir una conexión con la base de datos debemos utilizar el método getConnection de la clase DriverManager. El método getConnection recibe tres parámetros. Son ellos la URL de conexión JDBC, el usuario y la contraseña.
+
+        Statement statement = con.createStatement();
+
+        statement.execute("SELECT ID, NOMBRE, DESCRIPCION, CANTIDAD FROM PRODUCTO");
+
+        ResultSet resultSet = statement.getResultSet();
+
+        List<Map<String, String>> resultado = new ArrayList<>();
+
+        while (resultSet.next()) {
+            Map<String, String> fila = new HashMap<>();
+            fila.put("ID", String.valueOf(resultSet.getInt("ID")));
+            fila.put("NOMBRE", resultSet.getString("NOMBRE"));
+            fila.put("DESCRIPCION", resultSet.getString("DESCRIPCION"));
+            fila.put("CANTIDAD", String.valueOf(resultSet.getInt("CANTIDAD")));
+
+            resultado.add(fila);
+        }
+        
+        con.close();
+
+        return resultado;
+    }*/
+    
     public List<Map<String, String>> listar() throws SQLException {
         ConnectionFactory factory = new ConnectionFactory();
         Connection con = factory.recuperaConexion();
